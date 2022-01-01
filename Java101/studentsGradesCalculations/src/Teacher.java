80a31d58a373857d6b621baf6430ac896e8f6c28
Zeynep <zgurbilek16@ku.edu.tr>
2022-01-01 23:51:26 +0300

    first commit of student grade

---
+++ Java101/studentsGradesCalculations/src/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+    String name;
+    String mbno;
+    String branch;
+
+    public Teacher(String name, String mbno, String branch) {
+        this.name = name;
+        this.mbno = mbno;
+        this.branch = branch;
+    }
+    }
